a685bf5ccf64986dc94f37f40af732da4ba645b7
rameem2003 <rameem2019@gmail.com>
2022-05-22 10:05:56 +0600

    update

--- profile.php
+++ profile.php
@@ -10,6 +10,12 @@
         header("location:login.php");
     }
 
+    if(isset($_GET['logout'])){
+        unset($user_id);
+        session_destroy();
+        header("location:login.php");
+    }
+
 ?>
 
 
@@ -20,8 +26,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- local css -->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
-<body>
+<body class="bg-primary">
     <?php 
     
         $load_profile = "SELECT * FROM `user_data` WHERE id = '$user_id'";
@@ -35,6 +47,21 @@
 
         
         ?>
-        <h1><?php echo $row["name"] ?></h1>
+        
+        <div class="col-md-6 col-sm-12 profile bg-white rounded">
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <p class="mt-2">First name: <?php echo $row["first_name"]  ?> </p>
+                    <p class="mt-2">Date of Birth: <?php echo $row["dob"]  ?> </p>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <p class="mt-2">Last name: <?php echo $row["last_name"]  ?> </p>
+                    <p class="mt-2">Email: <?php echo $row["email"]  ?> </p>
+                </div>
+            </div>
+
+            <a class="btn btn-danger d-block mb-3" href="profile.php?logout=<?php echo $user_id ?>">Logout</a>
+            <a class="btn btn-warning d-block mb-3" href="#">Edit</a>
+        </div>
 </body>
 </html>
